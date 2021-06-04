ffe11e349be35519989a4fa1f397f3a22c5b354a
Sunny Singh <sunnysingh@MacBook-Pro.local>
2021-06-05 03:54:45 +0530

    updated menu-items to navigate withROuterHOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Route } from 'react-router';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => (
+  <h1>Hats</h1>
+);
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/hats' component={HatsPage} />
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,19 +1,24 @@
 import React from "react";
+import { withRouter } from "react-router";
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, history, match, linkUrl }) => {
   return (
-    <div className={`menu-item ${size}`}>
+    <div
+      className={`menu-item ${size}`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+      /* /somematchedURL/linkURL */
+    >
       <div
         className="background-image"
         style={{ backgroundImage: `url(${imageUrl})` }}
       />
-        <div className="content">
-          <h1 className="title">{title.toUpperCase()}</h1>
-          <span className="subtitle">SHOP NOW</span>
-        </div>
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
     </div>
   );
 };
 
-export default MenuItem;
+export default withRouter(MenuItem);
