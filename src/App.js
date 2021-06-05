5e2040a2f40ab9dc511d02dad80822aecbe557dc
Sunny Singh <sunnysingh@MacBook-Pro.local>
2021-06-05 06:27:00 +0530

    updating app with shop-component, collection-preview and collection-item

--- src/App.js
+++ src/App.js
@@ -2,16 +2,13 @@ import React from 'react';
 import { Route } from 'react-router';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-
-const HatsPage = () => (
-  <h1>Hats</h1>
-);
+import ShopPage from './pages/shop/shop.component'
 
 function App() {
   return (
     <div className="App">
       <Route exact path='/' component={HomePage} />
-      <Route exact path='/hats' component={HatsPage} />
+      <Route  path='/shop' component={ShopPage} />
     </div>
   );
 }
